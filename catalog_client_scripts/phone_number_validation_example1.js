51e29f471146374d6dec8165c9b76ea2872b38dd
= <=>
2016-10-02 09:55:15 -0500

    trying differnet things

--- catalog_client_scripts/phone_number_validation_example1.js
+++ catalog_client_scripts/phone_number_validation_example1.js
@@ -1,11 +1,18 @@
+/*global g_form*/
 function onChange(control, oldValue, newValue, isLoading) {
-  if(isLoading || newValue == ''){
-      return;
-  }
-  // Allows formats of (999) 999-9999, 999-999-9999, and 9999999999
-  var pattern = /^[(]?(\d{3})[)]?[-|\s]?(\d{3})[-|\s]?(\d{4})$/;
-  if(!pattern.test(newValue)){
-      g_form.showFieldMsg(control, 'Phone enter a valid phone number');
-      control.value = '';  
-  }
+    'use strict';
+    if (isLoading || newValue === '') { //if loading, or newvalue is empty
+        return;
+    }
+    var n = parseInt(newValue, 10);
+    console.log(n);
+    console.log(n.length);
+
+    if (parseInt(newValue, 10).toString().length === 10) { //ensure its 10 numbers
+        g_form.hideFieldMsg(control, true);
+        var newPhone = '(' + control.value.substr(0, 3) + ')' + control.value.substr(3, 3) + '-' + control.value.substr(6, 4);
+        control.value = newPhone;
+    } else {
+        g_form.showFieldMsg(control, 'The phone number must be in this format: 1234567890.', 'error');
+    }
 }
