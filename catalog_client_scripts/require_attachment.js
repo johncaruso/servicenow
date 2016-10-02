3014ee976028ed9ca8062b1ce67fbdddda7748f6
= <=>
2016-10-02 13:23:40 -0500

    On Submit, require an attachment script

---
+++ catalog_client_scripts/require_attachment.js
@@ -0,0 +1,12 @@
+/*global g_form, gel, GlideRecord, alert*/
+function onSubmit() {
+    'use strict';
+    var attachment = new GlideRecord("sys_attachment");
+    attachment.addQuery("table_name", "sc_cart_item");
+    attachment.addQuery("table_sys_id", gel('sysparm_item_guid').value);
+    attachment.query();
+    if (!attachment.hasNext()) {
+        alert("You must attach your public SSH Key. Please see instructions for more information.");
+        return false;
+    }
+}
